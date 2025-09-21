49e8c10f7c85dc8c88e7316664bf7df11f0d6199
freddedotme-v2 <code@fredde.me>
2025-09-21 10:22:57 +0200

    added ratelimit and slowdown

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import express from 'express'
+import rateLimit from 'express-rate-limit';
+import slowDown from 'express-slow-down';
 import item from './router/item.ts'
 
 const app = express()
@@ -10,6 +12,17 @@ if (!process.env.GQL_ENDPOINT) {
     console.log('| *GQL_ENDPOINT -> GraphQL URL endpoint')
     console.log('-----------------------------------------------------')
 } else {
+    app.use(rateLimit({
+        windowMs: 15 * 60 * 1000,
+        max: 100
+    }))
+
+    app.use(slowDown({
+        windowMs: 15 * 60 * 1000,
+        delayAfter: 25,
+        delayMs: () => 1000
+    }))
+
     app.use('/item', item)
     app.listen(port, () => {
         console.log(`Listening on port ${port}`)
